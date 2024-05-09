e43fbd3d6b3cf68d2ebb6893cf52f4e0e7bbafed
unknown <gagarinjoshua@gmail.com>
2024-05-09 13:45:23 +0800

    first commit

---
+++ features_labels.py
@@ -0,0 +1,72 @@
+class MyClass:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+        self.create_data = self.create_formatted_data(dictionary)
+        self.features = self.build_features(self.create_data)
+        self.labels = self.build_labels(self.create_data) # y labels
+        self.vocab = set()
+        self.idf = {}
+        self.fit(self.features)
+        self.x_features = self.transform(self.features) # X features
+        print(self.x_features)
+        print(self.labels)
+    def create_formatted_data(self,dictionary):
+        training_dict = {}
+        for intent, question_list in dictionary.items():
+            
+            for question in question_list:
+                training_dict[question] = intent
+                
+        return training_dict
+    
+    def build_features(self,create_data):
+        feature = np.array(list(create_data.keys()))
+        return feature
+    
+    def build_labels(self,create_data):
+        labels = np.array(list(create_data.values()))
+        reshaped_labels = labels.reshape(-1,1)
+        return reshaped_labels
+    
+    def fit(self, documents):
+        self.doc_count = len(documents)
+        # Build vocabulary and compute document frequency (DF)
+        for doc in documents:
+            terms = set(doc.split())
+            self.vocab.update(terms)
+            
+            for term in terms:
+                if term in self.idf:
+                    self.idf[term] += 1
+                else:
+                    self.idf[term] = 1
+        
+        # Compute IDF
+        for term in self.idf:
+            self.idf[term] = math.floor(math.log(self.doc_count / (1 + self.idf[term])))
+
+    def transform(self, documents):
+        tfidf_matrix = np.zeros((len(documents), len(self.vocab)))
+        term_index = {term: i for i, term in enumerate(sorted(self.vocab))}
+        
+        for i, doc in enumerate(documents):
+            term_freq = Counter(doc.split())
+            doc_len = len(doc.split())
+            
+            for term in term_freq:
+                if term in self.vocab:
+                    tf = term_freq[term] / doc_len
+                    tfidf_matrix[i, term_index[term]] = tf * self.idf[term]
+
+        return tfidf_matrix
+
+import json
+import numpy as np
+import math
+from collections import Counter
+
+with open("chatdata.json", 'r') as f:
+    chat_data = json.load(f)
+    f.close()
+
+my_dict = MyClass(chat_data)
